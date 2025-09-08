9614e9a33de0c4e32180838c6aa12cac2425fe60
Adr Ian <S9yN37@users.noreply.github.com>
2025-09-08 21:04:40 +0300

    Install osx-x64 / osx-arm64

--- Formula/subtitletools.rb
+++ Formula/subtitletools.rb
@@ -1,16 +1,24 @@
 class Subtitletools < Formula
   desc "A command-line tool for managing and synchronizing subtitle files"
   homepage "https://github.com/S9yN37/SubtitleTools"
-  url "https://github.com/S9yN37/SubtitleTools/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "b879a2bc19b77dd3aefe5ef43504cff0b184e44c641853631ae00f58e70517c3"
   version "1.0.0"
   license "MIT"
 
-  depends_on "python@3.13"
-  depends_on "python-setuptools"
+  on_macos do
+    on_intel do
+      url "https://github.com/S9yN37/SubtitleTools/releases/download/v1.0.0/SubtitleTools-v1.0.0-osx-x64.tar.gz"
+      sha256 "db1c12455796162741e5e85463cfa31b0f4230f5f37057d10dab366959b51e36"
+    end
+
+    on_arm do
+      url "https://github.com/S9yN37/SubtitleTools/releases/download/v1.0.0/SubtitleTools-v1.0.0-osx-arm64.tar.gz"
+      sha256 "ebfb68d74c776974dbf2879562d0e16b57fc6244fcdd427cbf9f9a208ad99592"
+    end
+  end
 
   def install
-    system "python3", *Language::Python.setup_install_args(prefix)
+    # Extract binary and rename to SubtitleTools
+    bin.install "SubtitleTools" # adjust if binary is inside a folder in tarball
   end
 
   test do
