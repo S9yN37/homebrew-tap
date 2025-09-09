26beba92dfb124ea1830f9db8b07187da26ec4c8
Adr Ian <S9yN37@users.noreply.github.com>
2025-09-09 10:42:02 +0300

    Update

--- Formula/subtitletools.rb
+++ Formula/subtitletools.rb
@@ -7,14 +7,12 @@ class Subtitletools < Formula
   on_macos do
     on_intel do
       url "https://github.com/S9yN37/SubtitleTools/releases/download/v1.0.0/SubtitleTools-v1.0.0-osx-x64.tar.gz"
-      sha256 "cd82b4470ab732e1e5be6721f722e53c37604e79a55eb11a3619a2047ec73b8e"
-      sha256 "db1c12455796162741e5e85463cfa31b0f4230f5f37057d10dab366959b51e36"
+      sha256 "TEST"
     end
 
     on_arm do
       url "https://github.com/S9yN37/SubtitleTools/releases/download/v1.0.0/SubtitleTools-v1.0.0-osx-arm64.tar.gz"
-      sha256 "1c25bf86d6db9433dcae42ea368500c8901c33b931ed9b012bfe0f182603f3c5"
-      sha256 "ebfb68d74c776974dbf2879562d0e16b57fc6244fcdd427cbf9f9a208ad99592"
+      sha256 "TEST"
     end
   end
 
